a02a8a65d740fde7f4ccb612556d7e8072995db6
Leandro Ribeiro <Leandro.Ribeiro@faro.com>
2023-03-03 15:52:22 +0000

    Almost Batman

---
+++ src/main/kotlin/AoC2022/aoc2022/Aoc2022Application.kt
@@ -0,0 +1,11 @@
+package AoC2022.aoc2022
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Aoc2022Application
+
+fun main(args: Array<String>) {
+	runApplication<Aoc2022Application>(*args)
+}
---
+++ src/test/kotlin/AoC2022/aoc2022/Aoc2022ApplicationTests.kt
@@ -0,0 +1,13 @@
+package AoC2022.aoc2022
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Aoc2022ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
